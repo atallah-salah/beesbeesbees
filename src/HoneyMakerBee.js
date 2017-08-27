8139c3055ff72c77561dc9996d8932ed37716172
jwanzerkani94 <zerkani-jwan@hotmail.com>
2017-08-26 20:14:26 -0400

    bees solution

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
 var HoneyMakerBee = function() {
+	Bee.call(this);
+	this.age=10;
+	this.job= 'make honey'
+	this.eat=Grub.prototype.eat;
+	this.honeyPot=0;
 };
+HoneyMakerBee.prototype.makeHoney = function(){
+	this.honeyPot++;
+}
 
+HoneyMakerBee.prototype.giveHoney = function(){
+	this.honeyPot--;
+}
+// HoneyMakerBee.prototype = Object.create(Bee.prototype);
+// HoneyMakerBee.prototype = Object.create(Grub.prototype);
+// HoneyMakerBee.prototype.constructor = HoneyMakerBee ;
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,14 @@
 var RetiredForagerBee = function() {
+ForagerBee.call(this);
+this.age=40;
+this.job='gamble';
+this.canFly=false;
+this.color='grey';
 };
 
+RetiredForagerBee.prototype.forage=function () {
+	return "I am too old, let me play cards instead"
+}
+
+RetiredForagerBee.prototype=Object.create(ForagerBee.prototype)
+RetiredForagerBee.prototype.constructor = RetiredForagerBee ;
